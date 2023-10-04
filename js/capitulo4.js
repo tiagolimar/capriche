3dc3104541ad33ec5a2a844444447ebc7d00eabb
tiagolimar <tiagolima.r2@gmail.com>
2023-10-04 13:06:44 -0300

    inicio da funcao cadastrar

--- js/capitulo4.js
+++ js/capitulo4.js
@@ -1,23 +1,39 @@
 import { CriadorDeConteudo } from "./capitulo3.js";
 
-`C://temp/caprichapp
+`C:\temp\caprichapp\
 
 
 `
 
 class Menu{
     constructor(){
-        cadastrar()
-        listar()
-        excluir()
-        descrever()
-        editar()
+        this.caminho = "C:\\temp\\caprichapp\\"
+    }
+    // listar()
+    // excluir()
+    // descrever()
+    // editar()
+
+    formatar(texto){
+        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "") + '.json'
+    }
+
+    salvar_em_arquivo(){
+        console.log("Arquivo salvo, parabéns!")
+    }
+
+    cadastrar(){
+        const conteudo = new CriadorDeConteudo()
+        const nome_do_arquivo = this.formatar(conteudo.titulo)
+        console.log(this.caminho + nome_do_arquivo);
+        this.salvar_em_arquivo()
     }
 }
 
 
 const main = ()=>{
     const menu = new Menu();
+    menu.cadastrar()
 }
 
 export default main;
