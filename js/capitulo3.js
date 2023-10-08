30075956569888919d8b461a6321c0aa30a3d209
tiagolimar <tiagolima.r2@gmail.com>
2023-10-08 12:53:11 -0300

    correcao de encoding

--- js/capitulo3.js
+++ js/capitulo3.js
@@ -1,69 +1,30 @@
-import read from 'readline-sync';
+import readline from 'readline';
 import clear from 'clear-console';
 
-read.setDefaultOptions({encoding:"utf-8"});
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-class Quiz{
-    constructor (conteudo_do_quiz){
+class Quiz {
+    constructor(conteudo_do_quiz) {
         this.titulo = conteudo_do_quiz.titulo;
         this.perguntas = conteudo_do_quiz.perguntas;
         this.respostas = conteudo_do_quiz.respostas;
-
-        this.introducao();
-        this.pontuacao = this.perguntar(this.perguntas);
-        this.analisar();
     }
 
-    introducao (){
+    async introducao() {
         console.log(`\n${this.titulo}, responda cada pergunta a seguir.\n`);
         console.log("Vamos começar? (pressione enter)\n\n");
-        read.question("");
-    }
-    
-    perguntar (){
-        let pontuacao_total = 0;
-        for (const pergunta of this.perguntas){
-            clear()
-            const {descricao,opcoes} = pergunta;
-            let descricoes_opcoes = opcoes.map(op=>op.descricao);
-
-            const index = read.keyInSelect(descricoes_opcoes,descricao);
-            let peso = opcoes[index].peso;
-            pontuacao_total += +peso;
-        }
-        return pontuacao_total
+        await this.question(" "); // Aguarda a entrada do usuário
     }
 
-    analisar (){
-        clear();
-        console.log('Pontuação Total = ',this.pontuacao, '\n');
-    
-        for (const [peso_maximo,descricao] of this.respostas){
-            if (this.pontuacao <= peso_maximo){
-                console.log(descricao)
-                return null
-            }
-        }
-    }
-}
-
-export class CriadorDeConteudo{
-    constructor(){
-        this.titulo = read.question("Qual é o título do questionário? ",{encoding:'utf-8'})
-
-        this.numero_de_perguntas = parseInt(read.question("Quantas perguntas serão feitas? "))
-        this.perguntas = [];
-        this.gerar_perguntas()
-    
-        clear()
-        this.numero_de_respostas = parseInt(read.question("Quantas respostas possíveis serão dadas? "))
-        this.gerar_respostas()
-
-        this.conteudo = {
-            titulo: this.titulo,
-            perguntas: this.perguntas,
-            respostas: this.respostas
-        }
+    async question(prompt) {
+        return new Promise((resolve) => {
+            rl.question(prompt, (input) => {
+                resolve(input);
+            });
+        });
     }
 
     obter_opcoes(){
@@ -84,36 +45,127 @@ export class CriadorDeConteudo{
         }
         return opcoes
     }
-    
-    gerar_perguntas(){
-        for (let i=0; i<this.numero_de_perguntas;i++){
-            let pergunta = {};
-            clear()
-            pergunta.descricao = read.question("Qual é a pergunta? ");
-            pergunta.opcoes = this.obter_opcoes();
-            this.perguntas.push({...pergunta})
+
+    imprimir_opcoes(array){
+        return array.map((opcao, indice) => `[${indice}] ${opcao.descricao}`).join('\n');
+    }
+
+    async perguntar() {
+        let pontuacao_total = 0;
+        for (const pergunta of this.perguntas) {
+            clear();
+            const { descricao, opcoes } = pergunta;
+            let descricoes_opcoes = opcoes.map(op => op.descricao);
+            this.imprimir_opcoes(opcoes)
+            const index = await this.question(descricao + " " + descricoes_opcoes.join(", ") + ": ");
+            let peso = opcoes[index].peso;
+            pontuacao_total += +peso;
+        }
+        return pontuacao_total;
+    }
+
+    analisar() {
+        clear();
+        console.log('Pontuação Total = ', this.pontuacao, '\n');
+
+        for (const [peso_maximo, descricao] of this.respostas) {
+            if (this.pontuacao <= peso_maximo) {
+                console.log(descricao);
+                return null;
+            }
         }
     }
-    
-    gerar_respostas(){
+
+    async iniciar() {
+        await this.introducao();
+        this.pontuacao = await this.perguntar();
+        this.analisar();
+        rl.close();
+    }
+}
+
+export class CriadorDeConteudo {
+    constructor() {
+        this.titulo = "";
+        this.numero_de_perguntas = 0;
+        this.numero_de_respostas = 0;
+        this.perguntas = [];
         this.respostas = new Map();
-        clear()
-        for (let i=0; i<this.numero_de_respostas;i++){
-            const descricao = read.question("Qual é a resposta? ");
-            const peso_maximo = read.question("Qual é o peso máximo da resposta? ");
-            this.respostas.set(peso_maximo,descricao);
+    }
+
+    async question(prompt) {
+        return new Promise((resolve) => {
+            rl.question(prompt, (input) => {
+                resolve(input);
+            });
+        });
+    }
+
+    async obter_opcoes(){
+        let opcoes = [];
+        const MAX_OPCOES = 5;
+        
+        for(let i=0;i<MAX_OPCOES;i++){
+            clear()
+            i>1 && console.log('Escreva "sair" para concluir');
+
+            let opcao = {}
+            opcao.descricao = await this.question(`Escreva a descrição da opção ${i+1}: `);
+            if (opcao.descricao.toLowerCase()=="sair" && i>1){
+                return opcoes
+            }
+            opcao.peso = await this.question(`Escreva o peso da opção ${i+1}: `)
+            opcoes.push({...opcao})
+        }
+        return opcoes
+    }
+
+    async gerar_perguntas() {
+        for (let i = 0; i < this.numero_de_perguntas; i++) {
+            let pergunta = {};
+            clear();
+            pergunta.descricao = await this.question("Qual é a pergunta? ");
+            pergunta.opcoes = await this.obter_opcoes();
+            this.perguntas.push({ ...pergunta });
         }
     }
 
-    get get_conteudo (){
-        return this.conteudo
+    async gerar_respostas() {
+        clear();
+        for (let i = 0; i < this.numero_de_respostas; i++) {
+            const descricao = await this.question("Qual é a resposta? ");
+            const peso_maximo = await this.question("Qual é o peso máximo da resposta? ");
+            this.respostas.set(peso_maximo, descricao);
+        }
+    }
+
+    async criarConteudo() {
+        this.titulo = await this.question("Qual é o título do questionário? ");
+        this.numero_de_perguntas = parseInt(await this.question("Quantas perguntas serão feitas? "));
+        await this.gerar_perguntas();
+
+        clear();
+        this.numero_de_respostas = parseInt(await this.question("Quantas respostas possíveis serão dadas? "));
+        await this.gerar_respostas();
+
+        this.conteudo = {
+            titulo: this.titulo,
+            perguntas: this.perguntas,
+            respostas: this.respostas
+        };
+    }
+
+    get get_conteudo() {
+        return this.conteudo;
     }
 }
 
-const main = ()=>{
+const main = async () => {
     let criador = new CriadorDeConteudo();
+    await criador.criarConteudo();
     const conteudo = criador.get_conteudo;
-    new Quiz(conteudo);
-}
+    let quiz = new Quiz(conteudo);
+    await quiz.iniciar();
+};
 
 export default main;
--- js/capitulo4.js
+++ js/capitulo4.js
@@ -1,39 +1,52 @@
 import { CriadorDeConteudo } from "./capitulo3.js";
+import fs from 'fs/promises'; // Importe o módulo fs.promises para operações de arquivo assíncronas.
 
-`C:\temp\caprichapp\
-
-
-`
-
-class Menu{
-    constructor(){
-        this.caminho = "C:\\temp\\caprichapp\\"
-    }
-    // listar()
-    // excluir()
-    // descrever()
-    // editar()
-
-    formatar(texto){
-        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "") + '.json'
+class Menu {
+    constructor() {
+        this.caminho = "C:\\temp\\caprichapp\\";
     }
 
-    salvar_em_arquivo(){
-        console.log("Arquivo salvo, parabéns!")
+    formatar(texto) {
+        const regex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|\/\-="']/g;
+        texto = texto.replace(regex, '')
+        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "") + '.json';
     }
 
-    cadastrar(){
-        const conteudo = new CriadorDeConteudo()
-        const nome_do_arquivo = this.formatar(conteudo.titulo)
-        console.log(this.caminho + nome_do_arquivo);
-        this.salvar_em_arquivo()
+    async salvar_em_arquivo(conteudo, nome_do_arquivo) {
+        try {
+            const caminho_completo = this.caminho + nome_do_arquivo;
+            await fs.writeFile(caminho_completo, JSON.stringify(conteudo, null, 2), 'utf8');
+            console.log("Arquivo salvo, parabéns!");
+        } catch (erro) {
+            console.error("Erro ao salvar o arquivo:", erro);
+        }
+    }
+
+    async gerar_conteudo(){
+        let criador = new CriadorDeConteudo();
+        await criador.criarConteudo();
+        return criador.get_conteudo;
+    }
+
+    async cadastrar() {
+        try {
+            let conteudo = await this.gerar_conteudo();
+            conteudo.respostas = Array.from(conteudo.respostas)
+            const titulo = conteudo.titulo;
+            if(titulo){
+                const nome_do_arquivo = this.formatar(titulo);
+                console.log(this.caminho + nome_do_arquivo);
+                await this.salvar_em_arquivo(conteudo, nome_do_arquivo);
+            }
+        } catch (erro) {
+            console.error("Erro ao cadastrar:", erro);
+        }
     }
 }
 
-
-const main = ()=>{
+const main = async () => {
     const menu = new Menu();
-    menu.cadastrar()
-}
+    await menu.cadastrar();
+};
 
 export default main;
